f5f2da420a730c24713ba64a3efa7290fb97c221
Robin Haider <robinhaider69@gmail.com>
2022-08-15 11:14:53 +0600

    event sync

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
 const cors = require('cors');
 const axios = require('axios');
 
@@ -11,39 +11,60 @@ app.use(cors());
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
- res.send(commentsByPostId[req.params.id] || []);
+  res.send(commentsByPostId[req.params.id] || []);
 });
-
 
 app.post('/posts/:id/comments', async (req, res) => {
- const commentId = randomBytes(4).toString('hex');
- const {content} = req.body;
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
- const comments = commentsByPostId[req.params.id] || [];
- comments.push({id: commentId, content, status: 'pending'});
+  const comments = commentsByPostId[req.params.id] || [];
 
- commentsByPostId[req.params.id] = comments;
+  comments.push({ id: commentId, content, status: 'pending' });
 
- await axios.post('http://localhost:4005/events', {
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
-        id: commentId,
-        content,
-        postId: req.params.id,
-        status: 'pending'
-    }
- });
- 
- res.status(201).send(comments);
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending',
+    },
+  });
+
+  res.status(201).send(comments);
 });
 
-// receive events
-app.post('/events', (req, res) => {
-    console.log('Received Events', req.body.type);
+app.post('/events', async (req, res) => {
+  console.log('Event Received:', req.body.type);
 
-    res.send({});
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
+  res.send({});
 });
 
 app.listen(4001, () => {
-    console.log('Comments Service Listing on 4001');
-})
+  console.log('Listening on 4001');
+});
--- event-bus/index.js
+++ event-bus/index.js
@@ -5,18 +5,32 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
-    const event = req.body;
+  const event = req.body;
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
+  events.push(event);
 
-    res.send({status: 'OK'});
-})
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4003/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: 'OK' });
+});
 
+app.get('/events', (req, res) => {
+  res.send(events);
+});
 
 app.listen(4005, () => {
-    console.log('Listening on 4005');
-})
+  console.log('Listening on 4005');
+});
