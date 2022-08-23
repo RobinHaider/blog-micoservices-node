3ab30ec7f36820809e5d3ba4781ff46e9dd8bb57
Robin Haider <robinhaider69@gmail.com>
2022-08-23 11:02:03 +0600

    kubernetes - comments, moderation, query deplment and service config

--- event-bus/index.js
+++ event-bus/index.js
@@ -15,15 +15,19 @@ app.post('/events', (req, res) => {
   axios.post('http://post-clusterip-srv:4000/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('http://localhost:4001/events', event).catch((err) => {
+  axios
+    .post('http://comments-clusterip-srv:4001/events', event)
+    .catch((err) => {
+      console.log(err.message);
+    });
+  axios.post('http://query-clusterip-srv:4002/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('http://localhost:4002/events', event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post('http://localhost:4003/events', event).catch((err) => {
-    console.log(err.message);
-  });
+  axios
+    .post('http://moderation-clusterip-srv:4003/events', event)
+    .catch((err) => {
+      console.log(err.message);
+    });
   res.send({ status: 'OK' });
 });
 
