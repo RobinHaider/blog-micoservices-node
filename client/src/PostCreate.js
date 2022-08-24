2f453237c9646473216119bccadcc728a4732d98
Robin Haider <robinhaider69@gmail.com>
2022-08-24 13:20:25 +0600

    kubernetes - ingress routes

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    await axios.post('http://posts.com/posts', {
+    await axios.post('http://posts.com/posts/create', {
         title
     });
     setTitle('');
--- posts/index.js
+++ posts/index.js
@@ -15,20 +15,21 @@ app.get('/posts', (req, res) => {
 });
 
 
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const {title} = req.body;
+app.post('/posts/create', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
-    posts[id] = {
-        id, title
-    };
+  posts[id] = {
+    id,
+    title,
+  };
 
-    await axios.post('http://event-bus-srv:4005/events', {
-      type: 'PostCreated',
-      data: { id, title },
-    });
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'PostCreated',
+    data: { id, title },
+  });
 
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
 // receive events
